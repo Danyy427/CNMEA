3f7a065d2e719d529a4f663dad2cf53f463a6c6f
Danyy427 <56359485+Danyy427@users.noreply.github.com>
2023-07-12 12:07:06 +0300

    Changes to compiling

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
-gcc src/NMEAReceive.c -I inc -c -o bin/NMEAReceive.o
-gcc src/NMEAReceive.c -I inc -c -o bin/NMEATransmit.o
-gcc src/NMEAReceive.c -I inc -c -o bin/NMEUtils.o
-ar rcs bin/libCNEMA.a bin/NMEAReceive.o bin/NMEATransmit.o bin/NMEUtils.o
+rm -rf ./bin
+mkdir ./bin
+gcc src/NMEAReceive.c -I./inc -c -o bin/NMEAReceive.o
+gcc src/NMEAReceive.c -I./inc -c -o bin/NMEATransmit.o
+gcc src/NMEAReceive.c -I./inc -c -o bin/NMEAUtils.o
+ar rcs bin/libCNMEA.a bin/NMEAReceive.o bin/NMEATransmit.o bin/NMEAUtils.o
